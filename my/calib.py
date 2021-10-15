7024d1950158cfe28db797f359740c93751fdca4
virajk <virajk@saksglobal.com>
2021-10-15 07:08:24 -0400

    temp updates

--- my/calib.py
+++ my/calib.py
@@ -8,37 +8,37 @@ import cv2
 
 chessboardSize = (9, 6)
 
-grayL = cv2.cvtColor(cv2.imread('images/imageL5.png'), cv2.COLOR_BGR2GRAY)
-grayR = cv2.cvtColor(cv2.imread('images/imageR5.png'), cv2.COLOR_BGR2GRAY)
+imgL = cv2.imread('images/imageL0.png')
+imgR = cv2.imread('images/imageR5.png')
+
+grayL = cv2.cvtColor(imgL, cv2.COLOR_BGR2GRAY)
+grayR = cv2.cvtColor(imgR, cv2.COLOR_BGR2GRAY)
 
 # Find the chess board corners
 retL, cornersL = cv2.findChessboardCorners(grayL, chessboardSize, None)
 retR, cornersR = cv2.findChessboardCorners(grayR, chessboardSize, None)
 
-print(np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1, 2))
-
 objp = np.zeros((chessboardSize[0] * chessboardSize[1], 3), np.float32)
 # print(objp)
 objp[:, :2] = np.mgrid[0:chessboardSize[0],0:chessboardSize[1]].T.reshape(-1, 2)
-print(objp)
+# print(objp.shape)
 objp = objp * 20
 # print(objp)
-
-exit(0)
+# exit(0)
 
 if retR and retL:
 
     for corner_l, corner_r in zip(cornersL, cornersR):
 
-        center_coordinates_l = (int(corner_l[0][0]), int(corner_l[0][1]))
-        cv2.circle(grayL, center_coordinates_l, radius=10, color=(255, 0, 0), thickness=2)
+        # Draw and display the corners
+        cv2.drawChessboardCorners(imgL, chessboardSize, cornersL, retL)
+        cv2.imshow('img left', imgL)
+        cv2.drawChessboardCorners(imgR, chessboardSize, cornersR, retR)
+        cv2.imshow('img right', imgR)
 
-        center_coordinates_r = (int(corner_r[0][0]), int(corner_r[0][1]))
-        cv2.circle(grayR, center_coordinates_r, radius=10, color=(255, 0, 0), thickness=2)
 
-
-cv2.imshow("LEFT", grayL)
-cv2.imshow("RIGHT", grayR)
+# cv2.imshow("LEFT", grayL)
+# cv2.imshow("RIGHT", grayR)
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
--- my/test.py
+++ my/test.py
@@ -1,9 +1,20 @@
 import numpy as np
 
-a = np.mgrid[0:4, 0:4]
+a = np.mgrid[0:9, 0:6]
+# b = np.mgrid[0:3, 0:3]
 # b = np.meshgrid[0:4, 0:4]
 
-print(a)
-print(a.shape)
+# print(a)
+#
+# a = a.T.reshape(-1, 2)
+# print(a)
 
-# print(b)
+objp = np.zeros((9 * 6, 3), np.float32)
+print(objp)
+print(objp.shape)
+objp[0:54, 0:2] = a.T.reshape(-1, 2)
+# print(objp[0:54, 0:3])
+
+# a = a.flatten(order='F')
+# print(a)
+# print(len(a))
